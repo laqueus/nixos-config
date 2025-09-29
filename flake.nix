5a2b365b64b08a177db9bf373a28ea21781ad3e8
laqueus <n/a>
2025-09-29 10:46:42 -0500

    Basic flake push

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+       ./configuration.nix
+       ({ pkgs, ...}: {
+	 environment.systemPackages = with pkgs; [
+	    neovim
+	    joplin-desktop
+	 ];
+       })
+     
+      ];
+     
+    };
+
+    
+  };
+}
